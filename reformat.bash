144d44f219bab22ac618232b05199e1c690494d0
Jamie Hannaford <jamie.hannaford@rackspace.com>
2015-09-21 17:44:39 +0200

    make things more quiet

--- reformat.bash
+++ reformat.bash
@@ -2,4 +2,5 @@
 
 command -v json >/dev/null 2>&1 || { echo >&2 "The json npm package is required, but it's not installed. Please install with `npm install -g json`"; exit 1; }
 
-find v1 -type f -name "*.json" -exec json -f {} -I \;
+find v1 -type f -name "*.json" -exec sh -c "json -f {} -Iq > /dev/null" \;
+echo "Everything has been reformatted";
